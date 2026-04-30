f74a73cf6b378fb7a84539d4190780f4fb0ce0a8
Muhammad Zahid Iqbal <zahid.bscs2019@uoch.edu.pk>
2026-04-30 15:10:27 +0500

    Fix formatting and base path

--- neurocore/vite.config.js
+++ neurocore/vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+plugins: [
+react(),
+tailwindcss(),
+],
+base: './'
 })
